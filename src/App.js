46fdd1e686d7b82cea05d444c43796eb78a57e44
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-04-22 11:38:52 +0100

    add useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from './Button';
 
 function App() {
+
+  const [value, setValue] = React.useState('')
+
+  const classValue = (classNameValue) => {
+    if (classNameValue === 'red') {
+      return classNameValue
+    }
+    if (classNameValue === 'blue') {
+      return classNameValue
+    }
+    if (classNameValue === 'brown') {
+      return classNameValue
+    }
+  }
+  const handleClick = (colour) => {
+    setValue(classValue(colour))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={value} data-testid='show'></div>
+      <ButtonPanel clickHandle={handleClick} />
     </div>
   );
+}
+
+const ButtonPanel = ({clickHandle}) => {
+  const panel = ({ name, color }) => < Button handle={clickHandle} name={name} color={color} />
+  return (
+    <div className='button-panel'>
+      {panel({name: 'red', color: 'red'})}
+      {panel({name: 'blue', color: 'blue'})}
+      {panel({name: 'brown', color: 'brown'})}
+    </div>
+  )
 }
 
 export default App;
