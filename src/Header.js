7f483f93a06d0d774f03158904766242bffdea5b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-04-25 17:16:15 +0100

    Add header which contains the links to change the body color

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Header.css'
+
+const Header = ({clickHandle}) => {
+	return (
+		<div className="header d-flex justify-content-between p-2 bg-secondary text-white">
+			<div className="head-title text-uppercase">
+				<strong>Box Color</strong>
+			</div>
+
+			<div className="head-list list-unstyled d-flex justify-content-between">
+			<li className='head-li pr-5 text-white w-100' onClick={()=>{clickHandle('blue')}}>Blue</li>
+			<li className='head-li pr-5 text-white w-100' onClick={()=>{clickHandle('red')}}>Red</li>
+			<li className='head-li pr-3 text-white w-100' onClick={()=>{clickHandle('brown')}}>Brown</li>
+			</div>
+		</div>
+	)
+}
+
+export default Header
