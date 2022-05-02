fbf7314aeacccd6a1a08698040e7926d79c15513
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-05-02 23:58:45 +0100

    Add className to give the nav a different text color

--- src/Header.js
+++ src/Header.js
@@ -7,11 +7,10 @@ const Header = ({clickHandle}) => {
 			<div className="head-title text-uppercase">
 				<strong>Box Color</strong>
 			</div>
-
 			<div className="head-list list-unstyled d-flex justify-content-between">
-			<li className='head-li pr-5 text-white w-100' onClick={()=>{clickHandle('blue')}}>Blue</li>
-			<li className='head-li pr-5 text-white w-100' onClick={()=>{clickHandle('red')}}>Red</li>
-			<li className='head-li pr-3 text-white w-100' onClick={()=>{clickHandle('brown')}}>Brown</li>
+				<li className='head-li blue1 pr-5 text-white w-100' onClick={()=>{clickHandle('blue')}}>Blue</li>
+				<li className='head-li red1 pr-5 text-white w-100' onClick={()=>{clickHandle('red')}}>Red</li>
+				<li className='head-li brown1 pr-3 text-white w-100' onClick={()=>{clickHandle('brown')}}>Brown</li>
 			</div>
 		</div>
 	)
