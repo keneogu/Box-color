f6f4c72d855a2a38d20e514cf73da2a1a9e7e657
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-05-03 08:48:01 +0100

    Changed header background and title text color

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,14 @@ import './styles/Header.css'
 
 const Header = ({clickHandle}) => {
 	return (
-		<div className="header d-flex justify-content-between p-2 bg-secondary text-white">
-			<div className="head-title text-uppercase">
+		<div className="header d-flex justify-content-between p-2 bg-light text-white">
+			<div className="head-title text-uppercase text-dark">
 				<strong>Box Color</strong>
 			</div>
 			<div className="head-list list-unstyled d-flex justify-content-between">
-				<li className='head-li blue1 pr-5 text-white w-100' onClick={()=>{clickHandle('blue')}}>Blue</li>
-				<li className='head-li red1 pr-5 text-white w-100' onClick={()=>{clickHandle('red')}}>Red</li>
-				<li className='head-li brown1 pr-3 text-white w-100' onClick={()=>{clickHandle('brown')}}>Brown</li>
+				<li className='head-li blue1 pr-5 w-100' onClick={()=>{clickHandle('blue')}}>Blue</li>
+				<li className='head-li red1 pr-5 w-100' onClick={()=>{clickHandle('red')}}>Red</li>
+				<li className='head-li brown1 pr-3 w-100' onClick={()=>{clickHandle('brown')}}>Brown</li>
 			</div>
 		</div>
 	)
